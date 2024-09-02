42122220e5dc8939109f72ed7c8de9b64da02877
abobafett-dev <77672564+abobafett-dev@users.noreply.github.com>
2024-09-02 20:53:53 +0300

    Настройка ci/cd, pre-commit c pytype, yamlfix и ruff, корректировка зависимостей с использованием uv dev-dependecies, создание тестового pyqt приложения

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from software-purchases!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        label = QLabel("OwO")
+        self.setMinimumSize(500, 500)
+        self.setCentralWidget(label)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
