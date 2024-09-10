40276938905b248379906aa4b286d599af74a81b
abobafett-dev <77672564+abobafett-dev@users.noreply.github.com>
2024-09-10 12:14:59 +0300

    changed the application configuration for correct access to resources

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,8 +1,16 @@
+import sys
+from pathlib import Path
+
+
 class AppConfig:
     """
     Configuration File
     """
+
     APP_NAME: str = "БФТ Закупки"
+
+    BASE_PATH: Path = Path(__file__).resolve().parent
+    PROJECT_ROOT: Path = Path(sys.argv[0]).resolve().parent
 
     @classmethod
     def initialize(cls) -> None:
@@ -10,4 +18,13 @@ class AppConfig:
         Perform any necessary initializations here, e.g.:
         - Loading settings from a file
         """
-        pass
+
+    @classmethod
+    def get_resource_path(cls, relative_path: str) -> str:
+        """
+        Returns the absolute path to a resource, useful for handling files in PyInstaller-built applications.
+
+        :param relative_path: Relative path to the resource.
+        :return: Absolute path to the resource.
+        """
+        return str(cls.PROJECT_ROOT / relative_path)
